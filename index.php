8978e15805c8b7cfa5c32ea3612fadde99e7224f
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-05-16 11:33:48 -0300

    Merge branch 'master' of github.com:gerMdz/alameda
    # Conflicts:
    #	README.md

--- index.php
+++ index.php
@@ -21,43 +21,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Iglesia de La Alameda,
      <?php echo $lema; ?>, Iglesia Bautista, Celebracion, Dios, Fe, Amistad, Arte,Libertad, Servicio, Misión, Mendoza">
+    <meta name="keywords" content="Iglesia de la Alameda, Celebracion, Dios, Fe, Amistad, Arte, Libertad, Servicio, Misión, Mendoza">
     <meta name="author" content="Iglesia de la Alameda">
 
     <meta property="og:title" content="Iglesia Alameda" />
     <meta property="og:type" content="website" />
 
     <meta property="og:url" content="https://iglesiaalameda.com/index.php" />
-<!--    <meta property="og:image" content="https://www.iglesiaalameda.com/imagenes/meta/">-->
+
     <meta property="og:image" content="https://iglesiaalameda.com/images/og/og-base.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Iglesia de la Alameda</title>
-    <link rel="icon" href="images/index.png" sizes="32x32" />
-    <link rel="icon" href="images/index192.png" sizes="192x192" />
-    <link rel="apple-touch-icon-precomposed" href="images/indexapple.png" />
+    <?php include 'favicon.php'?>
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="stream-ui-kit/assets/vendors/bootstrap/css/bootstrap.min.css">
 
     <!-- Bootstrap core CSS -->
 
+    <!-- Icon core  -->
+
     <link href="css/foundation-icons.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/743c9941eb.js" crossorigin="anonymous"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Icon core  -->
+
+    <!-- Style 3th core  -->
     <link href="css/mdb.min.css" rel="stylesheet">
     <link href="css/business-casual.min.css?v=<?php echo $version ?>" rel="stylesheet">
-    <link href="css/igles.css?v=<?php echo $version ?>" rel="stylesheet">
-
-    <link href="css/alameda/fontAlameda.css" rel="stylesheet">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/swiper.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="stream-ui-kit/assets/vendors/slick-carousel/slick.css">
+    <link rel="stylesheet" type="text/css" href="stream-ui-kit/assets/css/styles.css">
+    <!-- Style 3th core  -->
 
-<!--    <link href="css/awasome/web-fonts-with-css/css/fontawesome-all.min.css " rel="stylesheet">-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-    <!--<script src="js/awasome/fontawesome-all.min.js"></script>-->
+    <!-- Style Auto core  -->
+    <link href="css/igles.css?v=<?php echo $version ?>" rel="stylesheet">
+    <!-- Style Auto core  -->
 
-    <!-- Custom fonts for this template -->
+    <!-- Fonts  core  -->
+    <link href="css/alameda/fontAlameda.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700%7COpen+Sans:300,400,600,700" rel="stylesheet">
     <link href="css/fonts/univers/font.css?v=<?php echo $version ?>" rel="stylesheet">
-
+    <!-- Fonts  core  -->
 
     <!-- Custom styles for this template -->
 
