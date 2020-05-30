f8620e9976f42020847bb92ea93cc3940b328778
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-05-30 08:39:43 -0300

    Vuelta atras

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public_html/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
