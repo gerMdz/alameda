635b04f697bb183cbc6cec437d5917af86e82641
Gerardo J Montivero <gerardo.montivero@gmail.com>
2020-06-05 22:14:53 -0300

    avanza.php incluye taller de madurez
    Signed-off-by: Gerardo J Montivero <gerardo.montivero@gmail.com>

--- oportunidades/grupos-pequeños.php
+++ oportunidades/grupos-pequeños.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <?php $version = date('YmdHi'); ?>
 
-  <head>
+<head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,69 +16,75 @@
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
-      <meta property="og:image" content="https://www.iglesiaalameda.com/images/og/og-base.png">
+    <meta property="og:image" content="https://www.iglesiaalameda.com/images/og/og-base.png">
     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-      <link rel="icon" href="images/index.png" sizes="32x32" />
-      <link rel="icon" href="images/index192.png" sizes="192x192" />
-      <link rel="apple-touch-icon-precomposed" href="images/indexapple.png" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="icon" href="images/index.png" sizes="32x32"/>
+    <link rel="icon" href="images/index192.png" sizes="192x192"/>
+    <link rel="apple-touch-icon-precomposed" href="images/indexapple.png"/>
     <!-- Custom styles for this template -->
 
 
-      <link href="../css/igles.css?v=<?php echo $version ?>" rel="stylesheet">
+    <link href="../css/igles.css?v=<?php echo $version ?>" rel="stylesheet">
 
-      <link href="css/styles.css?v=<?php echo $version ?>" rel="stylesheet">
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link href="css/styles.css?v=<?php echo $version ?>" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css"
+          rel="stylesheet"/>
 
-  </head>
+</head>
 
-  <body id="page-top">
+<body id="page-top">
 
-  <?php
-  include '../nav.php';
-  ?>
+<?php
+include '../nav.php';
+?>
 
-  <!-- Masthead-->
-  <header class="masthead-oportunidades">
-      <div class="container h-100">
-          <div class="row h-100 align-items-center justify-content-center text-center">
-              <div class="col-lg-10 align-self-end">
-                  <h1 class="text-uppercase text-black-50 font-weight-bold">Grupos Pequeños <small class="text-realce-verde">Comunitarios</small></h1>
-                  <hr class="divider my-4" />
-              </div>
-              <div class="col-lg-8 align-self-baseline">
-                  <p class="text-black-50 font-weight-light mb-5">Que cada persona que llegue a la iglesia,
-                      se integre, permanezca, se desarrolle y se multiplique en un Grupo Pequeño.</p>
-                  <a class="btn btn-blue btn-xl js-scroll-trigger" href="https://docs.google.com/forms/d/e/1FAIpQLSfuKOBEFTWx-t_wiQR7CL8Gf443RxGeAYhP1nNETb7BSNkGuA/viewform" target="_blank">
-                      Sumate desde aquí
-                  </a>
-              </div>
-          </div>
-      </div>
-  </header>
+<!-- Masthead-->
+<header class="masthead-oportunidades m-5 p-5">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-sm-12 col-lg-12 ">
+                <a class="btn btn-blue btn-xl js-scroll-trigger"
+                   href="https://docs.google.com/forms/d/e/1FAIpQLSfuKOBEFTWx-t_wiQR7CL8Gf443RxGeAYhP1nNETb7BSNkGuA/viewform"
+                   target="_blank">
+                    Sumate desde aquí
+                </a>
+                <hr class="divider my-4"/>
+            </div>
 
+            <div class="col-lg-12 ">
+                <h1 class="text-uppercase text-black-50 font-weight-bold">Grupos Pequeños <small
+                            class="text-realce-verde">Comunitarios</small></h1>
+                <hr class="divider my-4"/>
+            </div>
+            <div class="col-lg-8 ">
+                <p class="text-black-50 font-weight-light mb-5">Que cada persona que llegue a la iglesia,
+                    se integre, permanezca, se desarrolle y se multiplique en un Grupo Pequeño.</p>
 
-
-      <hr class="m-0">
-
-
-
-
+            </div>
+        </div>
     </div>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+</header>
 
 
+<hr class="m-0">
 
-  <?php
-  include '../internos/footer.php'
-  ?>
 
-  </body>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+
+<?php
+include '../internos/footer.php'
+?>
+
+</body>
 
 </html>
