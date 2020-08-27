86773a21183ad271f40a45f6169df17d7d39c0f7
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-08-27 11:02:25 -0300

    pasos
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- camino-de-crecimiento.php
+++ camino-de-crecimiento.php
@@ -144,9 +144,9 @@ include_once('gerVendor/gerFunctions.php')
                 <div class="col-md-4">
                     <div class="pasos h-100">
                         <div class="card card-plain bg-white h-100">
-                            <div class="col-md-6 ml-auto mr-auto img-20">
+                            <div class="col-md-6 mx-auto img-20">
                                 <img src="/images/avanza/paso-1.jpg" alt="Iglesia de La Alameda"
-                                     class="img-raised rounded-circle img-fluid mx-auto ">
+                                     class="img-raised rounded-circle img-fluid  ">
                             </div>
                             <h4 class="card-title text-center text-alameda">Paso uno: Conviértete en miembro
                                 <br>
