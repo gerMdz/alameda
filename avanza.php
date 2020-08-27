91cb87aba13d37d55365f801412c58b1af3d3489
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-08-27 11:09:46 -0300

    pasos
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- avanza.php
+++ avanza.php
@@ -144,9 +144,9 @@ include_once('gerVendor/gerFunctions.php')
                 <div class="col-md-4">
                     <div class="pasos h-100">
                         <div class="card card-plain bg-white h-100">
-                            <div class="col-md-6 ml-auto mr-auto img-20">
+                            <div class="col-sm-4 mx-auto text-center img-20">
                                 <img src="/images/avanza/paso-1.jpg" alt="Iglesia de La Alameda"
-                                     class="img-raised rounded-circle img-fluid ">
+                                     class="img-raised rounded-circle img-fluid  ">
                             </div>
                             <h4 class="card-title text-center text-alameda">Paso uno: Conviértete en miembro
                                 <br>
@@ -164,7 +164,7 @@ include_once('gerVendor/gerFunctions.php')
                 <div class="col-md-4">
                     <div class="pasos h-100">
                         <div class="card card-plain bg-white h-100 ">
-                            <div class="col-md-6 ml-auto mr-auto img-20">
+                            <div class="col-sm-4 mx-auto text-center img-20">
                                 <img src="/images/avanza/paso-2.jpg" alt="Iglesia de La Alameda"
                                      class="img-raised rounded-circle img-fluid">
                             </div>
@@ -181,7 +181,7 @@ include_once('gerVendor/gerFunctions.php')
                 <div class="col-md-4">
                     <div class="pasos h-100">
                         <div class="card card-plain bg-white h-100">
-                            <div class="col-md-6 ml-auto mr-auto img-20">
+                            <div class="col-sm-4 mx-auto text-center img-20">
                                 <img src="/images/avanza/paso-3.jpg" alt="Iglesia de La Alameda"
                                      class="img-raised rounded-circle img-fluid">
                             </div>
