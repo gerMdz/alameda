e4389c0d4fd410d5a55dbed4b04ac873677b4f68
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-08-28 09:28:18 -0300

    profile pasos
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- camino-de-crecimiento.php
+++ camino-de-crecimiento.php
@@ -111,8 +111,8 @@ include_once('gerVendor/gerFunctions.php')
                     <div class="mx-auto bg-white p-5 text-center">
                         <h4 class="text-alameda text-center text-uppercase text-realce-alameda">
                             Unite a esta charla en línea
-                            <hr/>
                         </h4>
+                        <hr/>
                         <p class="mx-auto text-center ">
                             29 de agosto a las 18:00 hs.
                         </p>
@@ -148,7 +148,8 @@ include_once('gerVendor/gerFunctions.php')
                                 <img src="/images/avanza/paso-1.jpg" alt="Iglesia de La Alameda"
                                      class="img-raised rounded-circle img-fluid  ">
                             </div>
-                            <h4 class="card-title text-center text-alameda">Paso uno: Conviértete en miembro
+                            <h4 class="card-title text-center text-alameda sub-img">
+                                Paso uno: Conviértete en miembro
                                 <br>
                             </h4>
                             <div class="card-body">
@@ -168,7 +169,8 @@ include_once('gerVendor/gerFunctions.php')
                                 <img src="/images/avanza/paso-2.jpg" alt="Iglesia de La Alameda"
                                      class="img-raised rounded-circle img-fluid">
                             </div>
-                            <h4 class="card-title text-center text-alameda">Paso dos: Adopta nuevos hábitos.
+                            <h4 class="card-title text-center text-alameda sub-img">
+                                Paso dos: Adopta nuevos hábitos.
                                 <br>
                             </h4>
                             <div class="card-body">
@@ -185,7 +187,8 @@ include_once('gerVendor/gerFunctions.php')
                                 <img src="/images/avanza/paso-3.jpg" alt="Iglesia de La Alameda"
                                      class="img-raised rounded-circle img-fluid">
                             </div>
-                            <h4 class="card-title text-center text-alameda">Paso tres: Descubrí tu FORMA
+                            <h4 class="card-title text-center text-alameda sub-img">
+                                Paso tres: Descubrí tu FORMA
                                 <br>
                             </h4>
                             <div class="card-body">
@@ -199,178 +202,7 @@ include_once('gerVendor/gerFunctions.php')
         </div>
     </section>
 
-    <!-- Features -->
-    <!--    <section class="bg-light u-content-space">-->
-    <!--        <div class="container">-->
-    <!--            <header class="text-center w-md-50 mx-auto mb-8">-->
-    <!--                <h2 class="h1">Features</h2>-->
-    <!--                <p class="h5">Stream creative technology company providing key digital services for everyone.</p>-->
-    <!--            </header>-->
-    <!---->
-    <!--            <!-- Feature Blocks -->
-    <!--            <div class="row">-->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fab fa-grav"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">Excellent Features</h4>-->
-    <!--                    <p>Achieve virtually any look and layout design within one UI Kit solution.</p>-->
-    <!--                </div>-->
-    <!---->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fab fa-connectdevelop"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">Powerful API</h4>-->
-    <!--                    <p>We strive to figure out ways to help your audience grow through all platforms.</p>-->
-    <!--                </div>-->
-    <!---->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fas fa-cubes"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">Blockchain Based</h4>-->
-    <!--                    <p>Find what you need in one template and combine features at will.</p>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--            <!-- End Feature Blocks -->
-    <!---->
-    <!--            <hr class="my-8">-->
-    <!---->
-    <!--            <!-- Feature Blocks -->
-    <!--            <div class="row">-->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fab fa-vuejs"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">VueJS Included</h4>-->
-    <!--                    <p>Easy and fast adjustments of elements are possible with Stream UI Kit.</p>-->
-    <!--                </div>-->
-    <!---->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fab fa-react"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">React Powered</h4>-->
-    <!--                    <p>Our React Powered App will save you tons of time and cost in your projects.</p>-->
-    <!--                </div>-->
-    <!---->
-    <!--                <div class="col-lg-4 mb-5 mb-lg-0">-->
-    <!--                    <div class="display-4 text-primary mb-2">-->
-    <!--                        <i class="fab fa-angular"></i>-->
-    <!--                    </div>-->
-    <!--                    <h4 class="h5">Angular Compatible</h4>-->
-    <!--                    <p>Power your app with Angular components for no cost within one stop solution.</p>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--            <!-- End Feature Blocks -->
-    <!--        </div>-->
-    <!--    </section>-->
-    <!-- End Features -->
 
-    <!-- Portfolio -->
-    <!--    <section class="u-content-space">-->
-    <!--        <div class="container">-->
-    <!--            <header class="text-center w-md-50 mx-auto mb-8">-->
-    <!--                <h2 class="h1">Branding Works</h2>-->
-    <!--                <p class="h5">We design and develope experiances that make people's lives easy and simple.</p>-->
-    <!--            </header>-->
-    <!---->
-    <!--            <ul class="js-shuffle-controls u-portfolio-controls text-center mb-5">-->
-    <!--                <li class="u-portfolio-controls__item"><a href="#!" data-group="all" class="active">All</a></li>-->
-    <!--                <li class="u-portfolio-controls__item"><a href="#!" data-group="its-illustration">Illustration</a></li>-->
-    <!--                <li class="u-portfolio-controls__item"><a href="#!" data-group="its-design">Design</a></li>-->
-    <!--                <li class="u-portfolio-controls__item"><a href="#!" data-group="its-graphic">Graphic</a></li>-->
-    <!--                <li class="u-portfolio-controls__item"><a href="#!" data-group="its-logo">Logo</a></li>-->
-    <!--            </ul>-->
-    <!---->
-    <!--            <!-- Work Content -->
-    <!--            <div class="js-shuffle u-portfolio row no-gutters mb-6">-->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-illustration"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img1.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">IX Project</h6>-->
-    <!--                        <small class="d-block">Branding</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img1.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-design"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img2.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">Bottle Design</h6>-->
-    <!--                        <small class="d-block">Mockup</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img2.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-graphic"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img3.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">App Developement</h6>-->
-    <!--                        <small class="d-block">Åpp</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img3.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-logo"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img4.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">Just Bored</h6>-->
-    <!--                        <small class="d-block">Freetime</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img4.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-illustration"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img5.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">Cake Lab</h6>-->
-    <!--                        <small class="d-block">Graphic</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img5.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item" data-groups='["its-graphic"]'>-->
-    <!--                    <img class="u-portfolio__image" src="assets/img-temp/portfolio/img6.jpg" alt="Image Description">-->
-    <!--                    <figcaption class="u-portfolio__info">-->
-    <!--                        <h6 class="mb-0">NB Project</h6>-->
-    <!--                        <small class="d-block">Logo</small>-->
-    <!--                    </figcaption>-->
-    <!--                    <a class="js-popup-image u-portfolio__zoom" href="assets/img-temp/portfolio/img6.jpg">Zoom</a>-->
-    <!--                </figure>-->
-    <!---->
-    <!--                <!-- sizer -->
-    <!--                <figure class="col-sm-6 col-md-4 u-portfolio__item shuffle_sizer"></figure>-->
-    <!--            </div>-->
-    <!--            <!-- End Work Content -->
-    <!---->
-    <!--            <!-- Clints -->
-    <!--            <div class="text-center">-->
-    <!--                <ul class="list-unstyled row justify-content-center mb-2">-->
-    <!--                    <li class="col-sm-6 col-lg-2">-->
-    <!--                        <img class="u-clients__image img-fluid" src="assets/img-temp/partners/img2.png"-->
-    <!--                             alt="Image Description">-->
-    <!--                    </li>-->
-    <!--                    <li class="col-sm-6 col-lg-2">-->
-    <!--                        <img class="u-clients__image img-fluid" src="assets/img-temp/partners/img3.png"-->
-    <!--                             alt="Image Description">-->
-    <!--                    </li>-->
-    <!--                    <li class="col-sm-6 col-lg-2">-->
-    <!--                        <img class="u-clients__image img-fluid" src="assets/img-temp/partners/img4.png"-->
-    <!--                             alt="Image Description">-->
-    <!--                    </li>-->
-    <!--                    <li class="col-sm-6 col-lg-2">-->
-    <!--                        <img class="u-clients__image img-fluid" src="assets/img-temp/partners/img5.png"-->
-    <!--                             alt="Image Description">-->
-    <!--                    </li>-->
-    <!--                </ul>-->
-    <!--                <p>Got project? <a href="contacts.html">Request a free quote!</a></p>-->
-    <!--            </div>-->
-    <!--            <!-- End Clints -->
-    <!--        </div>-->
-    <!--    </section>-->
-    <!-- End Portfolio -->
 </main>
 
 <!-- Footer -->
@@ -384,27 +216,14 @@ include "footer.php";
 include('js-base-uikit.php');
 ?>
 <!-- End Call Us Modal Window -->
-
-<!-- JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) -->
-<!-- Global Vendor -->
-<script src="assets/vendors/jquery.min.js"></script>
-<script src="assets/vendors/jquery.migrate.min.js"></script>
-<script src="assets/vendors/popper.min.js"></script>
-<script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
-
 <!-- Components Vendor  -->
-<script src="assets/vendors/jquery.parallax.js"></script>
-<script src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
-<script src="assets/vendors/shuffle/jquery.shuffle.min.js"></script>
-
-<!-- Theme Settings and Calls -->
-<script src="assets/js/global.js"></script>
-
+<script src="stream-ui-kit/assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="stream-ui-kit/assets/vendors/shuffle/jquery.shuffle.min.js"></script>
 <!-- Theme Components and Settings -->
-<script src="assets/js/vendors/parallax.js"></script>
-<script src="assets/js/vendors/magnific-popup.js"></script>
-<script src="assets/js/vendors/shuffle.js"></script>
-<!-- END JAVASCRIPTS -->
+
+<script src="stream-ui-kit/assets/js/vendors/magnific-popup.js"></script>
+<script src="stream-ui-kit/assets/js/vendors/shuffle.js"></script>
+
 </body>
 <!-- End Body -->
 </html>
