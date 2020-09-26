93807b38243a1dca6cfa20f63e83cebb1ffb64c4
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-09-26 12:53:24 -0300

    corrección 26092020
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

--- contacto.php
+++ contacto.php
@@ -15,26 +15,31 @@ $title = 'Contacto';
  * @example pregunta si la cuarentena terminó
  */
 $finQ = false;
-include_once ('gerVendor/gerFunctions.php')
+include_once('gerVendor/gerFunctions.php')
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php
-    include ('meta-base.php');
-    include ('style-base-uikit.php')
+    include('meta-base.php');
+    include('style-base-uikit.php')
     ?>
-    
-<!--    <link href="css/styles.css?v=--><?php //echo $version ?><!--" rel="stylesheet">-->
-    <script src = "js/alameda.js"> </script>
 
+    <!--    <link href="css/styles.css?v=--><?php //echo $version ?><!--" rel="stylesheet">-->
+    <script src="js/alameda.js"></script>
 
 
     <style>
-        strong{font-weight: 400 !important}
-        .card-text{font-size: 1.1em !important;
-        font-weight: 600 !important;}
+        strong {
+            font-weight: 400 !important
+        }
+
+        .card-text {
+            font-size: 1.1em !important;
+            font-weight: 600 !important;
+        }
+
         .header {
             background-image: url('images/series/cabecera_index.jpg');
             background-size: cover;
@@ -42,6 +47,7 @@ include_once ('gerVendor/gerFunctions.php')
             position: relative;
 
         }
+
         .overlay {
             position: absolute;
             min-height: 100%;
@@ -51,11 +57,13 @@ include_once ('gerVendor/gerFunctions.php')
             background: rgba(255, 255, 255, 0.6);
 
         }
+
         img.resize {
-            width:10%; /* you can use % */
+            width: 10%; /* you can use % */
             height: auto;
         }
-        .btn-xl{
+
+        .btn-xl {
             padding: 1.25rem 1.25rem;
             font-size: 0.85rem;
             font-weight: 700;
@@ -65,14 +73,13 @@ include_once ('gerVendor/gerFunctions.php')
         }
 
 
-
     </style>
 
-<!--    <link href="css/styles.css?v=--><?php //echo $version ?><!--" rel="stylesheet">-->
-<!--    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />-->
-<!---->
-<!---->
-<!--    <link href="css/landings/heroic-features.css" rel="stylesheet">-->
+    <!--    <link href="css/styles.css?v=--><?php //echo $version ?><!--" rel="stylesheet">-->
+    <!--    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />-->
+    <!---->
+    <!---->
+    <!--    <link href="css/landings/heroic-features.css" rel="stylesheet">-->
 
 </head>
 
@@ -88,25 +95,20 @@ include 'nav.php';
     <div class="container h-100">
         <div class="row h-100 align-items-center justify-content-center text-center">
             <div class="col-lg-10 align-self-end">
+                <h1 class="text-realce-alameda">
+                    <small class="text-black">Bienvenido a la Alameda </small><br/> ¡Nos encantaría conocerte!
+                </h1>
                 <a class="btn btn-blue btn-round btn-xl js-scroll-trigger "
                    href="https://forms.gle/yMmDuFqn6mXkJ8Sb6"
                    target="_blank">
                     Completar Tarjeta de Conexión
                 </a>
-                <h1 class="text-uppercase text-realce-alameda">
-                    <small class="text-black">Bienvenido a la Alameda </small><br/> ¡Nos encataría conocerte!
-                </h1>
-
                 <div class="d-flex col-sm-12">
                     <hr class="my-auto flex-grow-1 divider col-sm-5">
                     <img src="/images/institucional/logo-gris-oscuro-2020.jpg"
                          class="col-sm-1 img-fluid rounded-circle" style="max-width: 100px" alt="Iglesia de La Alameda">
                     <hr class="col-sm-5 my-auto flex-grow-1">
                 </div>
-
-
-
-
             </div>
             <div class="col-lg-8 align-self-baseline">
                 <p class="text-black-75 font-weight-light mb-5">
@@ -128,14 +130,14 @@ include 'nav.php';
 
 
 <hr class="m-0">
-    <?php
-    include "footer.php";
-    ?>
+<?php
+include "footer.php";
+?>
 
-    <!-- Bootstrap core JavaScript -->
-    <?php
-    include('js-base-uikit.php');
-    ?>
+<!-- Bootstrap core JavaScript -->
+<?php
+include('js-base-uikit.php');
+?>
 
 </body>
 
