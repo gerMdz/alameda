310deb1df2b301852930385f55a2fe38e9e61f04
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2020-12-13 18:49:16 -0300

    Mision2021.php
    Signed-off-by: Gerardo J. Montivero <gerardo.montivero@gmail.com>

---
+++ Mision2021.php
@@ -0,0 +1,7 @@
+<html lang="es">
+<?php
+/* Esto producirá un error. Fíjese en el html
+ * que se muestra antes que la llamada a header() */
+header('Location: https://igle.ar/Mision2021');
+exit;
+?>
